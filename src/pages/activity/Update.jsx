6164035c42f15f1c08d52f8f8b6a4320662013e8
lhttanner <lht_ge@163.com>
2017-10-12 23:32:34 +0800

    1

--- src/pages/activity/Update.jsx
+++ src/pages/activity/Update.jsx
@@ -7,8 +7,8 @@ import {bindActionCreators} from 'redux';
 import {fetchData, receiveData} from '@/action';
 import { actbase,actsave,actload} from '../../axios';
 
-
-
+import 'moment/locale/zh-cn';
+import moment from 'moment';
 const { MonthPicker, RangePicker } = DatePicker;
 
 /*活动管理新增*/
@@ -23,23 +23,33 @@ class ActivityAddForms extends React.Component {
         confirmDirty: false,
         code : '',
         vals :[],
-        activity:{}
+        activity:{},
+        sign_range_date :[],
+        vote_range_date:[]
 
     };
 
     componentDidMount() {
-
         this.start({id:this.props.params.id});
     }
 
     start = (parm) => {
         this.setState({loading: true});
         actload(parm).then(res => {
-            debugger;
-            console.log(res.data.vals)
+            console.log(res.data)
+            const sign_range_date = new Array();
+            sign_range_date[0] = moment(res.data.apply_start_time);
+            sign_range_date[1] = moment(res.data.apply_end_time);
+            // let vote_range_date = [];
+            // vote_range_date[0] = res.data.vote_start_time;
+            // vote_range_date[1] = res.data.vote_end_time;
+
+            console.log(sign_range_date);
+            // console.log(vote_range_date);
             this.setState({
-                code : res.data.code,
-                vals : res.data.vals,
+                activity : res.data,
+                sign_range_date :[moment(res.data.apply_start_time), moment(res.data.apply_end_time)] ,
+                // vote_range_date : vote_range_date,
                 loading: false
             });
         });
@@ -67,17 +77,6 @@ class ActivityAddForms extends React.Component {
             }
         });
     };
-    // handleConfirmBlur = (e) => {
-    //     const value = e.target.value;
-    //     this.setState({ confirmDirty: this.state.confirmDirty || !!value });
-    // };
-    // checkConfirm = (rule, value, callback) => {
-    //     const form = this.props.form;
-    //     if (value && this.state.confirmDirty) {
-    //         form.validateFields(['confirm'], { force: true });
-    //     }
-    //     callback();
-    // };
     render() {
         const { getFieldDecorator } = this.props.form;
         const formItemLayout = {
@@ -114,7 +113,7 @@ class ActivityAddForms extends React.Component {
         };
         return (
         <div className="gutter-example">
-            <BreadcrumbCustom first="活动管理" second="活动新增" />
+            <BreadcrumbCustom first="活动修改" second="活动修改" />
             <Card  bordered={false} title="活动信息" extra={<Link to={'/app/activity/list'}><Button>返回</Button></Link>}>
                 <Form layout='horizontal' onSubmit={this.handleSubmit} style={{marginTop: 20}}>
                 <Row gutter={16}>
@@ -142,7 +141,7 @@ class ActivityAddForms extends React.Component {
                             hasFeedback
                         >
                             {getFieldDecorator('code', {
-                                initialValue:this.state.code,
+                                initialValue:this.state.activity.code,
                                 rules: [{
                                     required: true, message: '请输入活动编码!',
                                 }],
@@ -159,6 +158,7 @@ class ActivityAddForms extends React.Component {
                             hasFeedback
                         >
                             {getFieldDecorator('sign_range_date', {
+                                initialValue:this.state.activity.sign_range_date,
                                 rules: [{
                                     required: true, message: '请填写报名时间!',
                                 }],
@@ -174,6 +174,7 @@ class ActivityAddForms extends React.Component {
                             hasFeedback
                         >
                             {getFieldDecorator('vote_range_date', {
+                                initialValue:this.state.activity.vote_range_date,
                                 rules: [{
                                     required: true, message: '请填写投票时间!',
                                 }],
@@ -190,7 +191,7 @@ class ActivityAddForms extends React.Component {
                             initialValue={2}
                         >
                             {getFieldDecorator('vote_limit', {
-                                initialValue:5,
+                                initialValue:this.state.activity.vote_limit,
                                 rules: [{
                                     required: true, message: '每人每天可投票数!',
                                 }],
@@ -225,6 +226,7 @@ class ActivityAddForms extends React.Component {
                             hasFeedback
                         >
                             {getFieldDecorator('remark', {
+                                initialValue:this.state.activity.remark,
                                 rules: [],
                             })(
                                 <TextArea placeholder="" autosize={{ minRows: 6, maxRows: 10 }} style={{ width: '100%' }} />
@@ -236,6 +238,14 @@ class ActivityAddForms extends React.Component {
                     <FormItem {...tailFormItemLayout}>
                         <Link to={'/app/activity/list'}><Button>取消</Button></Link>
                         <Button type="primary" htmlType="submit" size={'default'} style={{marginLeft:20}}>确定</Button>
+                    </FormItem>
+
+                    <FormItem>
+                        {getFieldDecorator('id', {
+                            initialValue: this.state.activity.id
+                        })(
+                            <Input type="hidden"/>
+                        )}
                     </FormItem>
                 </Row>
             </Form>
